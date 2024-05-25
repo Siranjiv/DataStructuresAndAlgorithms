0b64c08ee9185db9c17c45dfe79009da2f5a4b2c
siranjiv <siranjiv@gmail.com>
2024-05-25 11:52:06 +0530

    Merge Code

--- WorkSpace/10. Merge Sort/Merge.cpp
+++ WorkSpace/10. Merge Sort/Merge.cpp
@@ -13,6 +13,8 @@ using namespace std;
         int leftArray[leftArraySize];
         int rightArray[rightArraySize];
 
+//Here we are sorting the arrays to sub arrays
+
         //This is for the left array
         for(int i = 0; i< leftArraySize; i++)
             leftArray[i] = array[leftIndex + i];
@@ -20,6 +22,37 @@ using namespace std;
         //This is for the right array
         for(int j = 0; j < rightArraySize; j++)
             rightArray[j] = array[midIndex + 1 + j];    //  midIndex + 1 will be the first item side of the right array
+            
+
+        int index = leftIndex;  //This is the first item in the main array
+        int i = 0;  //Keep track of the first sub array
+        int j = 0;  //Keep track of the second sub array
+
+        while(i < leftArraySize && j < rightArraySize) {    //This while loop will run until one of the 2 sub arrays are empty
+            if (leftArray[i] <= rightArray[j]) {
+                array[index] = leftArray[i];    //insert the left array i index value to main array
+                index++;    //move main array index by one
+                i++;    //move the left sub array index
+            } else {
+                array[index] = rightArray[j];   //insert the right array j index value to main array
+                index++;    //move main array index by one
+                j++;    //move the right sub array index
+            } 
+        }
+
+        while (i < leftArraySize) {         //This loop will work when the right array in empty and left array has values
+            array[index] = leftArray[i];
+            index++;
+            i++;
+        }
+
+        while (j < rightArraySize) {       //This loop will work when the left array in empty and left array has values
+            array[index] = rightArray[j];
+            index++;
+            j++;
+        }
+        
+
     }
  
  
@@ -28,11 +61,13 @@ int main() {
     int myArray[] = {1,3,7,8,2,4,5,6};
 
     int size = sizeof(myArray) / sizeof(myArray[0]);
-    int leftIndex = 0;
-    int rightIndex = size - 1;
-    int midIndex = (size / 2) - 1;
+    
+    //This is to calculate the indexes
+    int leftIndex = 0;  //The left most index will be zero
+    int rightIndex = size - 1;  //The right most index will be the size - 1
+    int midIndex = (size / 2) - 1;  //This will calculate the middle index
 
-
+    //Use Breakpoints to understand more
    merge(myArray, leftIndex, midIndex, rightIndex);
 
 
